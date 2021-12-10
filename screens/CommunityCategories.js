872a301219efbe7e4419efdb32a7bde42dd2c5ee
Vukani <vukani@posteo.net>
2021-12-10 05:27:42 -0500

    fetching categories from repo service

--- screens/CommunityCategories.js
+++ screens/CommunityCategories.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import { ActivityIndicator } from 'react-native';
 import Toast from 'react-native-simple-toast';
-import generalCategory from '../categories/DefaultCategories';
+import { check, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 import { ThemeContext } from '../utility_components/theme-context';
 import StyleSheetFactory from '../utility_components/styles.js';
@@ -17,6 +18,9 @@ export default ({ route, navigation }) => {
     const styleSheet = StyleSheetFactory.getSheet(themeContext.backgroundColor);
 
     const [searchString, setSearchString] = React.useState('');
+    const [loading, setLoading] = React.useState(true);
+    const [categories, setCategories] = React.useState([]);
+    const [page, setPage] = React.useState(1);
 
 
     const BackAction = () => (
@@ -24,29 +28,56 @@ export default ({ route, navigation }) => {
     );
 
     React.useEffect(() => {
+        fetch('http://10.0.2.2:3000', {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                // console.log(responseJson.sharedCategories[0]);
+                setCategories(responseJson.sharedCategories)
+                setPage(responseJson.page)
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error(error);
+                setLoading(false);
+            });
+    }, [])
 
-    })
+
+    return (
+        <Kitten.Layout style={{ flex: 1, backgroundColor: themeContext.backgroundColor }}>
+            <Kitten.TopNavigation
+                alignment="center"
+                style={{ backgroundColor: themeContext.backgroundColor }}
+                title='Community Categories'
+                accessoryLeft={BackAction}
+            />
+            {loading ? (
+                <Kitten.Layout style={styleSheet.loading_container}>
+                    <ActivityIndicator
+                        style={{ alignSelf: 'center' }}
+                        size="large"
+                        color="#800"
+                        animating={loading}
+                    />
+                </Kitten.Layout>
+            ) : (
+                <Kitten.Text>
+                    {page}
+                </Kitten.Text>
+            )}
 
 
-    return (<Kitten.Layout style={{ flex: 1, backgroundColor: themeContext.backgroundColor }}>
-
-        <Kitten.TopNavigation
-            alignment="center"
-            style={{ backgroundColor: themeContext.backgroundColor }}
-            title='Community Categories'
-            accessoryLeft={BackAction}
-        />
-        <Kitten.Input
+            {/* <Kitten.Input
             value={searchString}
             placeholder='Search'
             accessoryRight={SearchIcon}
             keyboardType={'visible-password'}
             secureTextEntry={true}
             onChangeText={nextValue => setSearchString(nextValue)}
-        />
-        <Kitten.Text>
-            COMM CATS
-        </Kitten.Text>
+        /> */}
 
-    </Kitten.Layout>)
+        </Kitten.Layout>
+    )
 }
